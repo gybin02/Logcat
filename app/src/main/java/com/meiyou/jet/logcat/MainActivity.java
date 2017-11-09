87ffb078a4ad61a3557b325bf4bc97609622449a
zhengxiaobin <zhengxiaobin@linggan.com>
2017-11-09 11:04:11 +0800

    1. add 测试方法；

--- app/src/main/java/com/meiyou/jet/logcat/MainActivity.java
+++ app/src/main/java/com/meiyou/jet/logcat/MainActivity.java
@@ -3,9 +3,10 @@ package com.meiyou.jet.logcat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.meiyou.framework.logcat.RemoteLogcatServer;
+import com.meiyou.framework.LogCatHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,31 +14,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        LogCatHelper logCatHelper = LogCatHelper.getInstance();
         if (BuildConfig.DEBUG) { //avoid execution on release, it is only for testing purpoise
-            RemoteLogcatServer logcatServer
-                    = new RemoteLogcatServer(
-                    8080,  //port to open connection
-                    5000,  //page reload rate (ms)
-                    getApplicationContext()
-            );
-            logcatServer.startServer();
+
+            logCatHelper.startServer(getApplicationContext());
+//            RemoteLogcatServer logcatServer
+//                    = new RemoteLogcatServer(
+//                    8080,  //port to open connection
+//                    5000,  //page reload rate (ms)
+//                    getApplicationContext()
+//            );
+//            logcatServer.startServer();
         }
-        
+
         findViewById(R.id.btn_toast).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Show Toast", Toast.LENGTH_SHORT).show();
             }
         });
-        
-        
-        
+
+
         findViewById(R.id.btn_error).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int i = 9/0;
+                int i = 9 / 0;
             }
         });
+        
+        
+        TextView tvShow = findViewById(R.id.tv_show);
+        tvShow.setText(logCatHelper.getServerIp(this));
     }
+
+
 }
