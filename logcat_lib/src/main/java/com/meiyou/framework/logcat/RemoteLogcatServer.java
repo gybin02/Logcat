901db924814ea450b0d197091e615928cd7ccffe
zhengxiaobin <zhengxiaobin@linggan.com>
2017-11-24 16:20:16 +0800

    1. add support v4;

--- logcat_lib/src/main/java/com/meiyou/framework/logcat/RemoteLogcatServer.java
+++ logcat_lib/src/main/java/com/meiyou/framework/logcat/RemoteLogcatServer.java
@@ -1,13 +1,11 @@
 package com.meiyou.framework.logcat;
 
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.util.Pair;
 import android.text.TextUtils;
 import android.util.Log;
@@ -161,16 +159,16 @@ public class RemoteLogcatServer implements Runnable {
                     );
 
             //Build notification
-            NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(context)
-                            .setSmallIcon(android.R.drawable.sym_def_app_icon)
-                            .setContentTitle(context.getString(R.string.title_notification))
-                            .setContentText(context.getString(R.string.notification_message) + ipAddress + ":" + getPort())
-                            .setContentIntent(resultPendingIntent);
-
-            //Send notification
-            ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(1, mBuilder
-                    .build());
+//            NotificationCompat.Builder mBuilder =
+//                    new NotificationCompat.Builder(context)
+//                            .setSmallIcon(android.R.drawable.sym_def_app_icon)
+//                            .setContentTitle(context.getString(R.string.title_notification))
+//                            .setContentText(context.getString(R.string.notification_message) + ipAddress + ":" + getPort())
+//                            .setContentIntent(resultPendingIntent);
+//
+//            //Send notification
+//            ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(1, mBuilder
+//                    .build());
 
         }
     }
